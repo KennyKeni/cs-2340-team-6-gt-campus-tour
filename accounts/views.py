a5bcd6f08134ec66ab1af9f399da66e6b6637962
Sayan Dahiya <sayan.dahiya@gmail.com>
2025-11-03 16:35:04 -0500

    feature/user-story-1: account creation

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def register(request):
+    """Handle user registration."""
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'accounts/register.html', {'form': form})
--- gttour/urls.py
+++ gttour/urls.py
@@ -22,5 +22,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('accounts/', include('accounts.urls')),
     path('campus/', include('campus.urls')),
 ]
